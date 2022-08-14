803fd6940d3656267a5c6c7608de7055f9cc82e5
yapex <yapex.yin@gmail.com>
2022-08-14 17:06:07 +0800

    优化disk_cache, 添加disk_cache的测试

---
+++ tests/test_disk_cache.py
@@ -0,0 +1,21 @@
+import json
+from ycache import disk_cache
+from loguru import logger
+
+logger.add('output/log/test_disk_cache_{time}.log', retention=1)
+
+
+class TestDiskCache:
+
+    def test_functional_disk_cache(self):
+        @ disk_cache()
+        def function_that_takes_long(*args, **kwargs):
+            res = json.dumps({"args": args, "kwargs": kwargs})
+            return res
+
+        result = function_that_takes_long(
+            1, 3, [2, 3], {2: 3}, a=2, b=3, c=[2, 3], d={2: 3})
+
+        expected = '{"args": [1, 3, [2, 3], {"2": 3}], "kwargs": {"a": 2, "b": 3, "c": [2, 3], "d": {"2": 3}}}'
+
+        assert result == expected
--- ycache/__init__.py
+++ ycache/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 import hashlib
 import pickle
@@ -10,16 +10,16 @@ from loguru import logger
 
 
 def pickled_lru_cache(maxsize=128, typed=False, ttl=10):
-    PICKLE_PROTOCOL: int = pickle.HIGHEST_PROTOCOL
-
-    class Result:
-        __slots__ = ('value', 'death')
-
-        def __init__(self, value, death):
-            self.value = value
-            self.death = death
-
     def _decorator(func):
+        PICKLE_PROTOCOL: int = pickle.HIGHEST_PROTOCOL
+
+        class Result:
+            __slots__ = ('value', 'death')
+
+            def __init__(self, value, death):
+                self.value = value
+                self.death = death
+
         @lru_cache(maxsize=maxsize, typed=typed)
         def cached_func_with_ttl(pickled_args, pickled_kwargs) -> Result:
             args = pickle.loads(pickled_args)
@@ -54,30 +54,21 @@ def pickled_lru_cache(maxsize=128, typed=False, ttl=10):
 
 def disk_cache(cache_dir='.temp', ttl=60*60*24):
     def _decorator(func):
-        PICKLE_PROTOCOL: int = pickle.HIGHEST_PROTOCOL
-
-        def md5(s) -> str:
-            m = hashlib.md5()
-            m.update(str(s).encode('utf-8'))
-            return m.hexdigest()
-
         def _make_key(namespace, *args, **kwargs):
-            pickled_args: bytes = pickle.dumps(args, PICKLE_PROTOCOL)
-            pickled_kwargs: bytes = pickle.dumps(kwargs, PICKLE_PROTOCOL)
-            key: str = f'{namespace}-{pickled_args}-{pickled_kwargs}'
-            return md5(key)
+            key = {'name': namespace, 'args': args, 'kwargs': kwargs}
+            return key
 
         @wraps(func)
         def _wrapper(*args, **kwargs):
-            key: str = _make_key(func.__name__, args, kwargs)
+            key = _make_key(func.__name__, args, kwargs)
             with Cache(cache_dir) as cache:
                 if not cache.get(key):
                     logger.debug(f'not hitted, recache key:[{key}]')
                     result = func(*args, **kwargs)
-                    cache.set(key, pickle.dumps(result), expire=ttl)
+                    cache.set(key, result, expire=ttl)
                     return result
                 else:
                     logger.debug(f'[{key}] hitted in cache')
-                    return pickle.loads(cache.get(key))
+                    return cache.get(key)
         return _wrapper
     return _decorator
